d25edc9d2fb0e3205dbdf8e55f1b67fab9909ac0
Ganea Andreea <deeaganea@gmail.com>
2022-11-27 06:01:29 +0200

    Added contact and about page

--- app.py
+++ app.py
@@ -230,6 +230,16 @@ def home():
         return render_template('index.html')
 
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 if __name__ == '__main__':
     app.run(debug=DEBUG)
 
